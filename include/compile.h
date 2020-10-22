590da2eb79a54f71eac6968dfd65d4a90c612367
csander@caltech.edu <csander@caltech.edu>
2020-10-22 22:47:34 +0000

    Fix #1

--- include/compile.h
+++ include/compile.h
@@ -6,10 +6,10 @@
 #include "ast.h"
 
 /**
- * Prints x86-64 assembly code that implements the given TeenyBASIC statement.
- * This function will be called on each statement in the TeenyBASIC program in order.
+ * Prints x86-64 assembly code that implements the given TeenyBASIC AST.
  *
- * @param node the statement to compile (either a PRINT, LET, label, GOTO, or IF)
+ * @param node the statement to compile.
+ *   The root node will be a SEQUENCE, PRINT, LET, IF, or WHILE.
  * @return true iff compilation succeeds
  */
 bool compile_ast(node_t *node);
